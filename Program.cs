06df192eaa92a835fd8f239ca4d36885ea66b5a7
Nasiba <nasyrova.nasiba@gmail.com>
2021-03-23 21:27:18 +0500

    finished create command

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace HomeworkLesson18
 {
@@ -25,6 +27,37 @@ namespace HomeworkLesson18
             public List<Person> GetPeople();
             public void Update(Person person);
             public void Delete(int id);
+        }
+        public class PeopleRepository : IRepository
+        {
+            public void Create(Person person)
+            {
+                using (var conn = new SqlConnection(ConString))
+                {
+                    var sql = @"INSERT INTO PEOPLE (LastName, FirstName, MiddleName, BirtDate) 
+                    VALUES (@LastName, @FirstName, @MiddleName, @BirthDate)";
+                    conn.Execute(sql, person);
+                }
+            }
+            public Person Get(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<Person> GetPeople()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Update(Person person)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete(int id)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
